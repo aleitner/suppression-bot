9b128b64ab75e38c41848ba48f49ebedc65aabe3
aleitner <leitnersalex@gmail.com>
2020-02-20 13:55:05 -0500

    Dai is too positive so let's suppress him

---
+++ bot.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+var Sentiment = require('sentiment');
+var sentiment = new Sentiment();
+var user_id = "314976210171985920"; // Dai's user id
+var suppression_count = 0;
+
+// Initialize Discord Bot
+var client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    if (msg.author.id != user_id) {
+        return
+    }
+
+    var result = sentiment.analyze(msg.content)
+
+    if (result.score >= 3) {
+        suppression_count += 1;
+        msg.delete(1000);
+        msg.channel.send(`Dai has been too positive. ${suppression_count} message(s) suppressed.`);
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
