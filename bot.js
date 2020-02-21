4bd4337b27cd3da2a90d0b83b6aed83d3f7bb3d6
aleitner <leitnersalex@gmail.com>
2020-02-21 11:06:19 -0500

    Make some additions

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 var Sentiment = require('sentiment');
 var sentiment = new Sentiment();
 var user_id = "314976210171985920"; // Dai's user id
-var suppression_count = 0;
 
 // Initialize Discord Bot
 var client = new Discord.Client();
@@ -18,11 +17,16 @@ client.on('message', msg => {
 
     var result = sentiment.analyze(msg.content)
 
-    if (result.score >= 5) {
-        suppression_count += 1;
+    msg.channel.send(`<@${user_id}> message sentiment analysis: ${result.comparative}`);
+
+    if (result.comparative > 1) {
+        msg.channel.send(`<@${user_id}> has been very positive. Are you sure what you are saying is genuine and sincere?`);
+    } else if (result.comparative > 4) {
         msg.delete(1000);
-        msg.channel.send(`Dai has been too positive. ${suppression_count} message(s) suppressed.`);
-    }
+        msg.channel.send(`<@${user_id}> has been too positive. ${suppression_count} message(s) suppressed.`);
+    } else if (result.comparative < -1 ) {
+        msg.channel.send(`That was quite negative <@${user_id}> are you feeling okay today?`);
+    } 
 });
 
 client.login(process.env.BOT_TOKEN);
